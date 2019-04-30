6fb83aaf76bf8901e6d9ef35d781e2dbcdc06000
Yorrmungand <edgarmarques@hotmail.com.br>
2019-04-30 16:05:29 -0300

    Added Testimony and News Basic Visual

--- client/src/App.js
+++ client/src/App.js
@@ -90,6 +90,52 @@ class Jornada extends Component {
   }
 }
 
+class DepoimentosNotícias extends Component {
+  render() {
+    return (
+      <div class="testimony-news">
+        <div id="testimony">
+          <h1>Depoimentos</h1>
+          <div class="card-container">
+            <div class="card">
+              <p>"TRECHO DO DEPOIMENTO"</p>
+              <a>clique</a>
+            </div>
+            <p>Maria, aluna de ciência da computação</p>
+            <div class="card">
+              <p>"TRECHO DO DEPOIMENTO"</p>
+              <a>clique</a>
+            </div>
+            <p>Maria, aluna de ciência da computação</p>
+            <div class="card">
+              <p>"TRECHO DO DEPOIMENTO"</p>
+              <a>clique</a>
+            </div>
+            <p>Maria, aluna de ciência da computação</p>
+          </div>
+        </div>
+        <div id="news">
+          <h1>Notícias</h1>
+          <div class="headline-container">
+            <div class="headline">
+              <h3>MANCHETE</h3>
+            </div>
+            <div class="headline">
+              <h3>MANCHETE</h3>
+            </div>
+            <div class="headline">
+              <h3>MANCHETE</h3>
+            </div>
+            <div class="headline">
+              <h3>MANCHETE</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
 class Page extends Component {
   constructor(props) {
     super(props)
@@ -102,6 +148,7 @@ class Page extends Component {
         <Banner />
         <Informações />
         <Jornada />
+        <DepoimentosNotícias />
       </div>
     )
   }
