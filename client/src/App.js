46e3aab01f8ebd6939a82051f9f31ac85297a22b
Yorrmungand <edgarmarques@hotmail.com.br>
2019-04-30 11:55:21 -0300

    Added Basic Responsive Banner

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
 
-class Navbar extends Component{
-  render(){
-    return(
+class Navbar extends Component {
+  render() {
+    return (
       <div class="navbar">
         <a>Parceiros</a>
         <a>Resultados</a>
@@ -17,14 +17,34 @@ class Navbar extends Component{
   }
 }
 
+class Banner extends Component {
+  render() {
+    return (
+      <div id="banner">
+        <div class="text-container">
+          <img src="#" alt="LOGO" />
+          <h1>Título Introdutótio</h1>
+          <p>PEQUENO TEXO QUE DÁ CONTINIDADE AO TÍTULO. Lorem ipsum dolor sit amet et delectus accomodare his consul copiosae legendos at vix ad putent delectus delicata usu. Vidit dissentiet eos cu eum an brute cpiosae hendrerit. Eos erant dolorum an. Per facer affert ut. Mei iisque mentitum moderatius cu. Sit munenre facilis accusam eu dicat falli consulatu at vis. Te facilisis mnesarchum qui poss omnium mediocritatem est cu.</p>
+        </div>
+        <div class="image-container">
+          <img src="#" alt="IMAGEM" id="image" />
+        </div>
+      </div>
+    )
+  }
+}
+
 class Page extends Component {
   constructor(props) {
-    super(props) 
+    super(props)
   }
 
-  render(){
-    return(
-      <Navbar />
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Banner />
+      </div>
     )
   }
 }
